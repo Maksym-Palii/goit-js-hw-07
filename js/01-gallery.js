5c522ab04f4f798dda11931777e355aa867f273a
Maksym-Palii <paliym_@ukr.net>
2023-01-02 11:43:27 +0200

    Перша задача.

---
+++ js/01-gallery.js
@@ -0,0 +1,45 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const containerGalleryEl = document.querySelector(".gallery");
+
+const imgMarkup = createGalleryItemMarcup(galleryItems);
+containerGalleryEl.insertAdjacentHTML("beforeend", imgMarkup);
+
+containerGalleryEl.addEventListener("click", onDivGalleryClick);
+
+function createGalleryItemMarcup(images) {
+  return images
+    .map((image) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+            <img
+            class="gallery__image"
+            src="${image.preview}"
+            data-source="${image.original}"
+            alt="${image.description}"
+            />
+        </a>
+    </div>
+    `;
+    })
+    .join("");
+}
+
+function onDivGalleryClick(evt) {
+  evt.preventDefault();
+  const isImageEl = evt.target.classList.contains("gallery__image");
+
+  if (!isImageEl) {
+    return;
+  }
+  const imageOriginalLink = evt.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${imageOriginalLink}">
+`);
+
+  instance.show();
+
+  console.log(imageOriginalLink);
+}
